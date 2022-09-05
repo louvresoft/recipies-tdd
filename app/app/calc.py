7017efa675ec8f3ebfb8690aeb02061095ba486a
marco_romeromon <marco.romero@eon.com.mx>
2022-09-05 13:58:22 -0500

    Se Test para ver si la base de datos esta ok

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,9 +4,10 @@ Calculadora de funciones
 
 
 def add(x, y):
-    """ Add x and y and return result"""
+    """Add x and y and return result"""
     return x + y
 
+
 def subtract(x, y):
-    """ Resta x from y and return result """
-    return  x - y
+    """Resta x from y and return result"""
+    return x - y
--- app/app/test.py
+++ app/app/test.py
@@ -4,15 +4,16 @@ Ejemplo de test
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
-    """ Test the calc module """
+    """Test the calc module"""
 
     def test_add_numbers(self):
-        """ Test adding numbers together."""
+        """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """ Test resta numbers."""
+        """Test resta numbers."""
         res = calc.subtract(15, 10)
         self.assertEqual(res, 5)
