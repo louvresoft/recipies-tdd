24796932d4facd4b6b00c5307dc84a823e923759
marco_romeromon <marco.romero@eon.com.mx>
2022-09-05 17:36:11 -0500

    se agrega validaciones apara los usuarios

--- app/core/models.py
+++ app/core/models.py
@@ -14,10 +14,20 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ Crear, guardar y regresar um usuario"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('El usuario debe de tener email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
+    def create_superuser(self, email, password):
+        """ Crear y regresar un nuevo superusuario """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
